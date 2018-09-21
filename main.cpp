49234fe636abaa32f2a79a7301c6026a13db7db6
mesterakos963 <43440251+mesterakos963@users.noreply.github.com>
2018-09-21 18:25:49 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,16 +4,13 @@ using namespace std;
 
 int main()
 {
-    //2 változó értékenek cseréje segédváltozó nélkül
-
     int a = 7;
-    int b = 1;
+    int b = 2;
 
-    a = a + b;
-    b = a - b;
-    a = a - b;
+    a = a * b;
+    b = a / b;
+    a = a / b;
 
     cout << "a=" << a << endl << "b=" << b << endl;
-
     return 0;
 }
